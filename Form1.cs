0c5d56fd2da83a2be61d6c46c161c9d4a05f695f
Personal <sandovalaxel277@gmail.com>
2026-02-05 01:00:11 -0600

    Mejoras en diccionario de escalas y mensajes de error
    El diccionario de escalas ahora es insensible a mayúsculas/minúsculas y se han añadido nuevas escalas menores y con bemoles. El mensaje de error muestra las opciones disponibles si la escala no se encuentra.

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,7 @@ namespace Escalas
 {
     public partial class Form1 : Form
     {
-        Dictionary<string, string[]> escalas = new Dictionary<string, string[]>
+        Dictionary<string, string[]> escalas = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase)
         {
             {"Do", new string[]{"Do", "Re", "Mi", "Fa", "Sol", "La", "Si"}},
             {"Re", new string[]{"Re", "Mi", "Fa#", "Sol", "La", "Si", "Do#"}},
@@ -14,7 +14,15 @@ namespace Escalas
             {"Fa", new string[]{"Fa", "Sol", "La", "Sib", "Do", "Re", "Mi"}},
             {"Sol", new string[]{"Sol", "La", "Si", "Do", "Re", "Mi", "Fa#"}},
             {"La", new string[]{"La", "Si", "Do#", "Re", "Mi", "Fa#", "Sol#"}},
-            {"Si", new string[]{"Si", "Do#", "Re#", "Mi", "Fa#", "Sol#", "La#"}}
+            {"Si", new string[]{"Si", "Do#", "Re#", "Mi", "Fa#", "Sol#", "La#"}},
+            // Agregadas: algunas escalas menores y claves con bemoles
+            {"La menor", new string[]{"La", "Si", "Do", "Re", "Mi", "Fa", "Sol"}},
+            {"Mi menor", new string[]{"Mi", "Fa#", "Sol", "La", "Si", "Do", "Re"}},
+            {"Re menor", new string[]{"Re", "Mi", "Fa", "Sol", "La", "Sib", "Do"}},
+            {"Sol menor", new string[]{"Sol", "La", "Sib", "Do", "Re", "Mib", "Fa"}},
+            {"Do menor", new string[]{"Do", "Re", "Mib", "Fa", "Sol", "Lab", "Sib"}},
+            {"Sib", new string[]{"Sib", "Do", "Re", "Mib", "Fa", "Sol", "La"}},
+            {"Mib", new string[]{"Mib", "Fa", "Sol", "Lab", "Sib", "Do", "Re"}}
         };
 
         public Form1()
@@ -31,7 +39,8 @@ namespace Escalas
             }
             else
             {
-                lblResultado.Text = "Escala no encontrada (usa Do-Si)";
+                var opciones = string.Join(", ", escalas.Keys);
+                lblResultado.Text = "Escala no encontrada. Opciones: " + opciones;
             }
         }
     }
